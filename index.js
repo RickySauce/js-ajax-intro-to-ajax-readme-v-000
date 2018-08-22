4301f5340c51392df7fe6ee601bbb50a0fd51691
Eric DeSimone <eric.desimone@cortland.edu>
2018-08-22 20:04:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ function getCommits(el) {
   req.send()
 }
 
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
